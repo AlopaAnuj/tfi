61cb57a2b99b36fa65b2dcaf447d4ca4caf7df0e
AnujAlopa <averma@amalgamrx.com>
2024-03-27 12:11:06 +0530

    updated the

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use("/api", routerConfig());
 
 const _dirname = path.dirname("");
 
-const buildPath = path.join(_dirname  , "./reactclient/build");
+const buildPath = path.resolve("./reactclient/build");
 app.use(express.static(buildPath))
 
 
